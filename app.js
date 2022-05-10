9df73d3fb63bb09effe44d2a8c29472e91471084
elocanelon <eloyecanelonj@gmail.com>
2022-05-10 11:41:33 -0400

    Webserver en linea

---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+app.set('view engine', 'hbs');
+const port = process.env.PORT
+
+  // Handlebars
+app.get('/', function (req, res) {
+  res.render('home', {
+    titulo: "Prueba",
+    objeto : "De node"
+
+  })
+  })
+
+app.get('/generic', function (req, res) {
+  res.render('generic')
+})
+
+app.get('/elements', function (req, res) {
+  res.render('element')
+  })
+
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+app.use( express.static('public'))
+
+
+app.get('/generic', function (req, res) {
+  res.sendFile( __dirname + '/public/generic.html')
+  })
+
+app.get('/elements', function (req, res) {
+  res.sendFile( __dirname + '/public/elements.html')
+  })
+
+app.get('*', function (req, res) {
+  res.sendFile( __dirname + '/public/template/error-404.html')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
